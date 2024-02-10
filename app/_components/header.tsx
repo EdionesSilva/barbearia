0a0cc81901a0bcdef47bc18f8b1f0081fb199b8b
Ediones <ediones.contato@hotmail.com>
2024-02-10 17:18:28 -0300

    feat add confirmed bookings are show on home page

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -7,6 +7,7 @@ import { MenuIcon } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { useSession } from "next-auth/react";
 import SideMenu from "./side-menu";
+import Link from "next/link";
 
 const Header = () => {
     const {data, status} = useSession();
@@ -14,7 +15,9 @@ const Header = () => {
     return ( 
     <Card>
         <CardContent className="px-5 py-8 justify-between items-center flex flex-row">
-            <Image src="/Logo.png" alt="FSW Barber" height={22} width={120}/>
+            <Link href="/">
+                <Image src="/Logo.png" alt="FSW Barber" height={22} width={120}/>
+            </Link>
             <Sheet>
                 <SheetTrigger asChild>
                     <Button variant="outline" size="icon">
