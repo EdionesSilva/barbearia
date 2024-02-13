3179db0a9b5f33e1c4d7ade8646bda7e962e0af3
Ediones <ediones.contato@hotmail.com>
2024-02-13 00:58:11 -0300

    style: change colors, logo and favicon

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,9 +1,11 @@
 const Footer = () => {
-    return ( 
-        <div className="w-full bg-secondary py-6 px-5">
-            <p className="text-gray-400 text-xs font-bold opacity-75">© 2024 Copyright E-Tech</p>
-        </div>
-     );
+    return (
+        <footer>
+            <div className="w-full bg-secondary py-6 px-5">
+                <p className="text-gray-400 text-xs font-bold opacity-75">© 2024 Copyright E-Tech</p>
+            </div>
+        </footer>
+    );
 }
- 
+
 export default Footer;
--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -10,28 +10,30 @@ import SideMenu from "./side-menu";
 import Link from "next/link";
 
 const Header = () => {
-    const {data, status} = useSession();
+    const { data, status } = useSession();
 
-    return ( 
-    <Card>
-        <CardContent className="px-5 py-8 justify-between items-center flex flex-row">
-            <Link href="/">
-                <Image src="/Logo.png" alt="FSW Barber" height={22} width={120}/>
-            </Link>
-            <Sheet>
-                <SheetTrigger asChild>
-                    <Button variant="outline" size="icon">
-                        <MenuIcon size={18} />
-                    </Button>
-                </SheetTrigger>
+    return (
+        <header>
+            <Card>
+                <CardContent className="px-5 py-8 justify-between items-center flex flex-row">
+                    <Link href="/">
+                        <Image src="/logo-etech.png" alt="E-Tech Barber" height={22} width={120} />
+                    </Link>
+                    <Sheet>
+                        <SheetTrigger asChild>
+                            <Button variant="outline" size="icon">
+                                <MenuIcon size={18} />
+                            </Button>
+                        </SheetTrigger>
 
-                <SheetContent className="p-0">
-                    <SideMenu/>
-                </SheetContent>
-            </Sheet>
-        </CardContent>
-    </Card> 
+                        <SheetContent className="p-0">
+                            <SideMenu />
+                        </SheetContent>
+                    </Sheet>
+                </CardContent>
+            </Card>
+        </header>
     );
 }
- 
+
 export default Header;
